d27a745beeaadb902755d995841c57fca83dc010
yeaykbsjv <kskvsjvfj@atomicmail.io>
2026-07-29 06:09:21 +0330

    Update error messages and proxy port in start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ -z "$SOCKS_USER" ] || [ -z "$SOCKS_PASS" ]; then
-    echo "ERROR: SOCKS_USER and SOCKS_PASS variables are required"
-    exit 1
+  echo "Missing variables"
+  exit 1
 fi
 
-echo "Starting SOCKS5 proxy..."
+echo "Starting SOCKS5 on port 443"
 
 /microsocks/microsocks \
--p 1080 \
+-p 443 \
 -u "$SOCKS_USER" \
 -P "$SOCKS_PASS"
