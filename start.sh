83b8bb8c6e667e89d54676adb2505e14a2e4ee13
yeaykbsjv <kskvsjvfj@atomicmail.io>
2026-07-29 06:29:43 +0330

    Remove user credentials check in start.sh
    Removed check for SOCKS_USER and SOCKS_PASS before starting microsocks.

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if [ -z "$SOCKS_USER" ] || [ -z "$SOCKS_PASS" ]; then
-    echo "Missing SOCKS_USER or SOCKS_PASS"
-    exit 1
-fi
-
-echo "Starting microsocks..."
-
 /microsocks/microsocks \
 -i 0.0.0.0 \
 -p 1080 \
